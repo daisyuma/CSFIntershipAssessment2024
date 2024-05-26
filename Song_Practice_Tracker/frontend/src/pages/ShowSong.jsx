1febde979d2a4f937be34e3441d145ccdf649c29
daisyuma <dma@appneta.com>
2024-05-26 14:42:57 -0700

    added more spotify api calls

--- Song_Practice_Tracker/frontend/src/pages/ShowSong.jsx
+++ Song_Practice_Tracker/frontend/src/pages/ShowSong.jsx
@@ -9,6 +9,7 @@ const CLIENT_SECRET = "51b450fb42694ce2958687758a68d64b";
 const ShowSong = () => {
   const [searchInput, setSearchInput] = useState("");
   const [accessToken, setAccessToken] = useState("");
+  const [albums, setAlbums] = useState([])
 
   useEffect(() => {
     // var auth = {
@@ -40,12 +41,67 @@ const ShowSong = () => {
     // fetch('https://accounts.spotify.com/api/token', auth)
     .then((response) => {
       console.log(response.data.access_token)
+      setAccessToken(response.data.access_token)
     })
+    .then(() =>{
+      setSearchInput("Hillsong United")
+    })
+    .then(search())
   }, [])
 
   //Search
   async function search() {
+    console.log("line 53 " + searchInput)
     console.log("Searching for "+ searchInput); 
+    //Get Artist ID
+    var artistParameters = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + accessToken
+      }
+    }
+    // var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', artistParameters)
+    // .then(response => response.json())
+    // .then(data => console.log(data))
+    var artistId = await axios
+    .get('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist',
+    {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`
+      }
+    }
+    )
+    .then((response) => {
+        return response.data.artists.items[0].id
+    }
+    )
+    console.log('artistId: ' + artistId);
+    var returnedAlbums  = 
+    // await fetch('https://api.spotify.com/v1/artists/' + artistId + '/albums' + '?include_groups=album&market=US&limit=50', artistParameters)
+    // .then(response => response.json())
+    await axios
+    .get('https://api.spotify.com/v1/artists/' + artistId + '/albums' + '?include_groups=album&market=US&limit=50',
+    {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`
+      }
+    }
+    )
+    .then((response) => {
+      setAlbums(response.data.items)
+      console.log(response.data)
+      return response.data
+    }
+    );
+    //Get all albums with Artist ID
+
+    // Display all albums
+     
   }
   return (
     <div>ShowSong</div>
